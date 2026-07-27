89f3496b0447577869a4d562b072d6f21f148fa2
Lucortez28 <lucortez2872@gmail.com>
2026-07-27 00:46:34 -0300

    Add products data to products.js

---
+++ data/products.js
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: 1,
+    name: "Produto personalizado 3D",
+    description: "Peça criada sob medida conforme a necessidade do cliente.",
+    material: "PLA / PETG",
+    image: "/produto1.jpg"
+  },
+  {
+    id: 2,
+    name: "Placa decorativa 3D",
+    description: "Decorações exclusivas com alto nível de detalhes.",
+    material: "PLA",
+    image: "/produto2.jpg"
+  },
+  {
+    id: 3,
+    name: "Brindes corporativos",
+    description: "Peças personalizadas para empresas e eventos.",
+    material: "PLA",
+    image: "/produto3.jpg"
+  }
+];
